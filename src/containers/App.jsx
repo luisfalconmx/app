7d47c78d1b2683d5cd57e4c3674103c73f53f5b9
luisfalconmx <me@luisfalconmx.com>
2021-01-23 14:56:11 -0600

    feat: footer component added

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,12 +3,14 @@ import '../assets/styles/App.scss'
 import Navbar from '../components/Navbar'
 import Search from '../components/Search'
 import Card from '../components/Card'
+import Footer from '../components/Footer'
 
 const App = () => (
   <div className="App">
     <Navbar />
     <Search />
     <Card />
+    <Footer />
   </div>
 )
 
