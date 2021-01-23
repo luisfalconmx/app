6d08b0818344fa4b509b35e320a3f855ec5a069e
luisfalconmx <me@luisfalconmx.com>
2021-01-23 15:42:57 -0600

    feat: support for images with file-loader plugin

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../assets/styles/components/Card.scss'
+import Cover from '../assets/static/cover.png'
 
 const Card = () => (
   <div className="card">
     <a className="card__link" href="#">
       <div className="card__cover">
-        <img className="card__image" src="https://i.redd.it/tk46u5nrkxm21.png" alt=""/>
+        <img className="card__image" src={Cover} alt=""/>
       </div>
       <div className="card__content">
         <h2 className="card__title">Business stakeholders review wireframes to ensure that</h2>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import '../assets/styles/components/Navbar.scss'
+import Logo from '../assets/static/logo.png'
+import IconAccount from '../assets/static/icon-account.png'
 
 const Navbar = () => (
   <header className="navbar">
     <div className="navbar__brand">
-      <img src="https://res.cloudinary.com/luisfalconmx/image/upload/v1611272549/LuisFalconMX/luisfalconmx-logotipo.png" alt="LuisFalconMX - Logotipo Oficial"/>
+      <img src={Logo} alt="LuisFalconMX - Logotipo Oficial"/>
       <h1 className="navbar__title">Playground</h1>
     </div>
     <div className="navbar__account">
-        <img src="https://res.cloudinary.com/luisfalconmx/image/upload/v1611276063/LuisFalconMX/icon-account.png" alt="Icono de la cuenta"/>
+        <img src={IconAccount} alt="Icono de la cuenta"/>
         <strong>Iniciar Sesión</strong>
     </div>
   </header>
