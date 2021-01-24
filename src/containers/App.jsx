6afb9387d7a99aa77289c88b614a7eb2509a168f
luisfalconmx <me@luisfalconmx.com>
2021-01-24 16:54:46 -0600

    feat: create a fake api with json-server

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../assets/styles/App.scss'
 import Navbar from '../components/Navbar'
 import Search from '../components/Search'
 import Card from '../components/Card'
 import Footer from '../components/Footer'
 
-const App = () => (
-  <div className="App">
-    <Navbar />
-    <Search />
-    <Card />
-    <Footer />
-  </div>
-)
+const App = () => {
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/initalState')
+      .then(response => response.json())
+      .then(data => setVideos(data))
+  }, [])
+
+  console.log(videos)
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Search />
+      <Card />
+      <Footer />
+    </div>
+  )
+}
 
 export default App
